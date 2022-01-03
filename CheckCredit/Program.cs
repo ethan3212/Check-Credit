35614b48cfb37e3829905b595bb953f76725c977
ethan3212 <74456094+ethan3212@users.noreply.github.com>
2022-01-03 08:35:45 -0600

    Add files via upload

---
+++ CheckCredit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace CheckCredit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price;
+            const double LIMIT = 8000;
+            Console.Write("Please enter the price: ");
+            price = Convert.ToDouble(Console.ReadLine());
+
+            if (price > LIMIT)
+                Console.WriteLine("You have exceed the credit limit of ${0}", LIMIT);
+            else
+                Console.WriteLine("Approved ");
+        }
+    }
+}
